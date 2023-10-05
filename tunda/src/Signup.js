92b389e4a32fce2394094fd5e1e563161f78b6e7
SamuelWango <wangos478@gmail.com>
2023-10-05 15:58:30 +0300

    second

--- tunda/src/Signup.js
+++ tunda/src/Signup.js
@@ -1,12 +1,46 @@
-// Signup.js
-
-import React from 'react';
+import React, { useState } from 'react';
+import './index.css';
 
 function Signup() {
+  const [email, setEmail] = useState('');
+  const [telephone, setTelephone] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSignup = () => {
+    // Perform signup logic here (e.g., send data to backend)
+    alert('You have successfully subscribed!');
+    // Redirect to the homepage (assuming it's at '/')
+    window.location.href = '/';
+  };
+
   return (
-    <div>
-      <h2>Signup Page</h2>
-      {/* Add content for the Signup page */}
+    <div className="signup-container"> {/* Apply the signup-container class */}
+      <h2>Join Us </h2>
+      <div>
+        <label>Email:</label>
+        <input 
+          type="email" 
+          value={email} 
+          onChange={(e) => setEmail(e.target.value)} 
+        />
+      </div>
+      <div>
+        <label>Telephone:</label>
+        <input 
+          type="tel" 
+          value={telephone} 
+          onChange={(e) => setTelephone(e.target.value)} 
+        />
+      </div>
+      <div>
+        <label>Password:</label>
+        <input 
+          type="password" 
+          value={password} 
+          onChange={(e) => setPassword(e.target.value)} 
+        />
+      </div>
+      <button onClick={handleSignup}>Subscribe</button>
     </div>
   );
 }
